62e9f2a2d91c0fb70fd58503b318e40e6ecc37c6
Willian de Morais <williandmorais@gmail.com>
2016-11-16 22:58:46 -0200

    Adicionado method refresh e login

--- admin/source/services/auth.jsx
+++ admin/source/services/auth.jsx
@@ -4,8 +4,14 @@ import API from './api.jsx';
 class AuthEndpoint extends API {
     login(username, password){
         let url =  this.prepareUrl('auth/login/');
+        let credentials = btoa(`${username}:${password}`);
 
-        return this.post(url, null, auth=false, extra_settings={username: username, password: password});
+        return this.post(url, null, false, {'Authorization': `Basic ${credentials}`});
+    }
+
+    refresh() {
+        let url = this.prepareUrl('auth/refresh/');
+        return this.post(url);
     }
 }
 
