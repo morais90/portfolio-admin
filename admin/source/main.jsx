4281aae87dbd07c1dc2513887f2f3896f1d75cc3
Willian de Morais <williandmorais@gmail.com>
2016-11-20 08:41:00 -0200

    Modificado hooks

--- admin/source/main.jsx
+++ admin/source/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import { authRequired, authCheck } from './core/hooks.js';
 import Admin from './components/admin/admin.jsx';
 import Login from './components/login/login.jsx';
 import Dashboard from './components/dashboard/dashboard.jsx';
@@ -9,22 +10,6 @@ import Essay from './components/essay/essay.jsx';
 import Account from './components/account/account.jsx';
 import UserBoard from './components/user/user.jsx';
 
-
-const authRequired = (nextstate, replace) => {
-    let credentials =  localStorage.getItem('credentials');
-
-    if (!credentials) {
-        replace('/login');
-    }
-}
-
-const authCheck = (nextstate, replace) => {
-    let credentials = localStorage.getItem('credentials');
-
-    if (credentials) {
-        replace('/');
-    }
-}
 
 render((
     <Router history={browserHistory}>
