b2d3634c51596f724d8da5c4e07723a6ca3155df
Willian de Morais <williandmorais@gmail.com>
2016-11-20 08:41:19 -0200

    Configurado modulo para os hooks

---
+++ admin/source/core/hooks.js
@@ -0,0 +1,17 @@
+const authRequired = (nextstate, replace) => {
+    let credentials =  localStorage.getItem('token');
+
+    if (!credentials) {
+        replace('/login');
+    }
+}
+
+const authCheck = (nextstate, replace) => {
+    let credentials = localStorage.getItem('token');
+
+    if (credentials) {
+        replace('/');
+    }
+}
+
+export {authRequired, authCheck}
