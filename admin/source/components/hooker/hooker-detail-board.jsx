86bb9f691a944afab47dc371ca86b23fd0b48e7a
Willian de Morais <williandmorais@gmail.com>
2016-12-26 08:18:28 -0200

    Iniciado HookerDetailBoard

---
+++ admin/source/components/hooker/hooker-detail-board.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+import HookerEndpoint from '../../services/hooker.js';
+
+
+class HookerDetailBoard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default withRouter(HookerDetailBoard);
